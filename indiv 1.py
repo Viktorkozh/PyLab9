8533bf3c4478d1de643b1ab8cadfed2e6b3e6936
ViktorKozh <viktor.ru253@gmail.com>
2023-12-12 18:56:38 +0300

    Убрал данные для примера, заменил лист из даты рождения на объект datetime

--- indiv 1.py
+++ indiv 1.py
@@ -2,25 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from datetime import date
+from datetime import datetime
 
 
 if __name__ == '__main__':
     # Список людей со строками для демонстрации.
-    people = [
-        {'name': 'Иван', 'surname': 'Иванов', 'date_of_birth': [
-            1, 2, 1990], 'zodiac_sign': 'Овен'},
-        {'name': 'Мария', 'surname': 'Петрова', 'date_of_birth': [
-            15, 7, 1985], 'zodiac_sign': 'Рак'},
-        {'name': 'Алексей', 'surname': 'Сидоров', 'date_of_birth': [
-            10, 11, 2000], 'zodiac_sign': 'Скорпион'},
-        {'name': 'Елена', 'surname': 'Козлова', 'date_of_birth': [
-            3, 4, 1995], 'zodiac_sign': 'Овен'},
-        {'name': 'Сергей', 'surname': 'Игнатов', 'date_of_birth': [
-            22, 9, 1982], 'zodiac_sign': 'Дева'},
-        {'name': 'Анна', 'surname': 'Кузнецова', 'date_of_birth': [
-            5, 12, 1988], 'zodiac_sign': 'Стрелец'}
-    ]
+    people = []
 
     # Организовать бесконечный цикл запроса команд.
     while True:
@@ -35,8 +22,7 @@ if __name__ == '__main__':
             # Запросить данные о человеке.
             name = input("Фамилия: ")
             surname = input("Имя: ")
-            date_of_birth = list(map(int, input(
-                "Введите дату рождения (в формате ДД.ММ.ГГГГ через точку): ").split('.')))
+            date_of_birth = datetime.strptime(input("Введите дату рождения (в формате ДД.ММ.ГГГГ через точку): "), '%d.%m.%Y')
             zodiac_sign = input("Знак зодиака: ")
 
             # Создать словарь.
@@ -76,8 +62,7 @@ if __name__ == '__main__':
             # Вывести данные о всех людях.
             for idx, person in enumerate(people, 1):
                 # Преобразование даты рождения в строку для вывода
-                birth_date_str = '.'.join(
-                    map(str, person.get('date_of_birth', '')))
+                birth_date_str = person.get('date_of_birth').strftime('%d.%m.%Y')
                 print(
                     '| {:^4} | {:<20} | {:<20} | {:<15} | {:<13} |'.format(
                         idx,
@@ -100,7 +85,7 @@ if __name__ == '__main__':
             count = 0
             # Проверить данные о людях из списка.
             for person in people:
-                if person.get('date_of_birth', [])[1] == month:
+                if person.get('date_of_birth').month == month:
                     count += 1
                     print(
                         '{:>4}: {} {}'.format(count, person.get(
